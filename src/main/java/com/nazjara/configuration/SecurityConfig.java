eaef8f130a3ada9fd5c236a962db3cd9faa2ee41
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2021-03-02 22:26:02 +0200

    Add custom in-memory UserDetailsService

--- src/main/java/com/nazjara/configuration/SecurityConfig.java
+++ src/main/java/com/nazjara/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nazjara.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,13 +13,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/webjars/**", "/resources/**").permitAll()
-                    .antMatchers("/beers/find", "/beers*").permitAll()
-                    .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                    .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
+                .antMatchers("/", "/webjars/**", "/resources/**").permitAll()
+                .antMatchers("/beers/find", "/beers*").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
                 .anyRequest()
-                    .authenticated().and()
-                    .formLogin().and()
-                    .httpBasic();
+                .authenticated().and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    //    Custom in-memory UserDetailsService (1)
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("username")
+//                .password("password")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin);
+//    }
+
+
+    //    Custom in-memory UserDetailsService (2)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("username")
+                .password("{noop}password") //specifying a password encoder
+                .roles("ADMIN");
     }
 }
