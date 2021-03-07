5c9290d467b095ba28a37ebb2be599d9dfa86c93
Nazjara <nazjara@gmail.com>
2021-03-07 02:43:50 +0200

    Add custom authentication processing filter

--- src/main/java/com/nazjara/configuration/SecurityConfig.java
+++ src/main/java/com/nazjara/configuration/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.nazjara.configuration;
 
+import com.nazjara.security.RestHeaderAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+        var filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,6 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(restHeaderAuthFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
+
         http
                 .authorizeRequests()
                 .antMatchers("/", "/webjars/**", "/resources/**").permitAll()
--- src/test/java/com/nazjara/web/controllers/BeerRestControllerIntegrationTest.java
+++ src/test/java/com/nazjara/web/controllers/BeerRestControllerIntegrationTest.java
@@ -3,12 +3,32 @@ package com.nazjara.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class BeerRestControllerIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void deleteBeer() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/91f3101e-fe3d-4795-93fd-1ac4b6c76b95")
+            .header("Api-Key", "user1")
+            .header("Api-Secret", "password1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerBadCredentials() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/91f3101e-fe3d-4795-93fd-1ac4b6c76b95")
+                .header("Api-Key", "user10")
+                .header("Api-Secret", "password10"))
+                .andExpect(status().isUnauthorized());
+    }
 
     @Test
     void findBeers() throws Exception{
